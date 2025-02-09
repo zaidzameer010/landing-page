aa16da8378ae72b36c848c76c3546b960db1fb93
KJ <kjamal0104@icloud.com>
2025-02-09 14:13:38 +0400

    html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,10 @@
 import { Routes } from '@angular/router';
-import { Component } from '@angular/core';
-
-@Component({
-  template: `
-    <div class="w-full h-screen">
-      <iframe 
-        src="https://zohosecurepay.in/checkout/una5guwh-93am98fk7a9vc/Self-Paced-Program-One-Time--INR-35000"
-        class="w-full h-full border-0"
-        allow="payment"
-        loading="eager"
-      ></iframe>
-    </div>
-  `,
-  styles: [`
-    :host {
-      display: block;
-      width: 100%;
-      height: 100vh;
-      overflow: hidden;
-    }
-  `]
-})
-export class IframeComponent {}
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'win',
-    component: IframeComponent
-  },
-  {
-    path: 'thankyou',
-    loadChildren: () => import('./pages/thankyou/thankyou.routes').then(m => m.THANKYOU_ROUTES)
   },
   {
     path: 'home',
--- src/app/pages/thankyou/thankyou.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ThankYouComponent } from './thankyou.component';
-
-export const THANKYOU_ROUTES: Routes = [
-  {
-    path: '',
-    component: ThankYouComponent
-  }
-]; 
